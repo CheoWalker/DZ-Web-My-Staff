ec47f9b67a418cf337832bc65e0456f02a0b8f89
unknown <Irving@.(none)>
2014-10-07 12:18:24 -0400

    Updated the loading of custom post templates.

--- includes/dzweb_mystaff_function.php
+++ includes/dzweb_mystaff_function.php
@@ -17,6 +17,183 @@ function dzweb_mystaff_scripts() {
     wp_enqueue_script( 'dzweb_mystaff_js', plugin_dir_path( __FILE__ ) . '/js/dzweb_mystaff.js', true);
 }
 add_action( 'wp_enqueue_scripts', 'dzweb_mystaff_scripts' );
+
+/* Load Custom Post's Page Templates */
+
+class PageTemplater {
+
+        /**
+         * A Unique Identifier
+         */
+         protected $plugin_slug;
+
+        /**
+         * A reference to an instance of this class.
+         */
+        private static $instance;
+
+        /**
+         * The array of templates that this plugin tracks.
+         */
+        protected $templates;
+
+
+        /**
+         * Returns an instance of this class. 
+         */
+        public static function get_instance() {
+
+                if( null == self::$instance ) {
+                        self::$instance = new PageTemplater();
+                } 
+
+                return self::$instance;
+
+        } 
+
+        /**
+         * Initializes the plugin by setting filters and administration functions.
+         */
+        private function __construct() {
+
+                $this->templates = array();
+
+
+                // Add a filter to the attributes metabox to inject template into the cache.
+                add_filter(
+                    'page_attributes_dropdown_pages_args',
+                     array( $this, 'register_project_templates' ) 
+                );
+
+
+                // Add a filter to the save post to inject out template into the page cache
+                add_filter(
+                    'wp_insert_post_data', 
+                    array( $this, 'register_project_templates' ) 
+                );
+
+
+                // Add a filter to the template include to determine if the page has our 
+                // template assigned and return it's path
+                add_filter(
+                    'template_include', 
+                    array( $this, 'view_project_template') 
+                );
+
+
+                // Add your templates to this array.
+                $this->templates = array(
+                        'page-office.php'     => 'Office Page',
+                        'page-department.php'     => 'Department Page',
+                        'page-staff.php'     => 'Staff Page',
+                );
+                
+        } 
+
+
+        /**
+         * Adds our template to the pages cache in order to trick WordPress
+         * into thinking the template file exists where it doens't really exist.
+         *
+         */
+
+        public function register_project_templates( $atts ) {
+
+                // Create the key used for the themes cache
+                $cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
+
+                // Retrieve the cache list. 
+                // If it doesn't exist, or it's empty prepare an array
+                $templates = wp_get_theme()->get_page_templates();
+                if ( empty( $templates ) ) {
+                        $templates = array();
+                } 
+
+                // New cache, therefore remove the old one
+                wp_cache_delete( $cache_key , 'themes');
+
+                // Now add our template to the list of templates by merging our templates
+                // with the existing templates array from the cache.
+                $templates = array_merge( $templates, $this->templates );
+
+                // Add the modified cache to allow WordPress to pick it up for listing
+                // available templates
+                wp_cache_add( $cache_key, $templates, 'themes', 1800 );
+
+                return $atts;
+
+        } 
+
+        /**
+         * Checks if the template is assigned to the page
+         */
+        public function view_project_template( $template ) {
+
+                global $post;
+
+                if (!isset($this->templates[get_post_meta( 
+                    $post->ID, '_wp_page_template', true 
+                )] ) ) {
+                    
+                        return $template;
+                        
+                } 
+
+                $file = plugin_dir_path(__FILE__). get_post_meta( 
+                    $post->ID, '_wp_page_template', true 
+                );
+                
+                // Just to be safe, we check if the file exist first
+                if( file_exists( $file ) ) {
+                        return $file;
+                } 
+                else { echo $file; }
+
+                return $template;
+
+        } 
+
+
+} 
+
+add_action( 'plugins_loaded', array( 'PageTemplater', 'get_instance' ) );
+
+/* Load Custom Post's Templates */
+if( !function_exists('get_office_template') ):
+ function get_office_template($single_template) {
+    global $wp_query, $post;
+    if ($post->post_type == 'office'){
+        $single_template = plugin_dir_path(__FILE__) . 'includes/office.php';
+    }//end if office
+    return $single_template;
+}//end get_office_template function
+endif;
+ 
+add_filter( 'single_template', 'get_office_template' ) ;
+
+if( !function_exists('get_department_template') ):
+ function get_department_template($single_template) {
+    global $wp_query, $post;
+    if ($post->post_type == 'department'){
+        $single_template = plugin_dir_path(__FILE__) . 'includes/department.php';
+    }//end if department
+    return $single_template;
+}//end get_department_template function
+endif;
+
+add_filter( 'single_template', 'get_department_template' ) ;
+
+if( !function_exists('get_staff_template') ):
+ function get_staff_template($single_template) {
+    global $wp_query, $post;
+    if ($post->post_type == 'staff'){
+        $single_template = plugin_dir_path(__FILE__) . 'includes/staff.php';
+    }//end if staff
+    return $single_template;
+}//end get_staff_template function
+endif;
+ 
+add_filter( 'single_template', 'get_staff_template' ) ;
  
 /* Plugin Required */ 
 
